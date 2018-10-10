b0833ab1b24307ef996667b61df6c4d46d0a98cf
Robson <robson09tt@gmail.com>
2018-10-10 20:43:56 -0300

    Adicionada a inserção de dados no banco de dados

--- twipin/views.py
+++ twipin/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.models import User
 from .models import Twip
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
    contexto = {} 
    return render(request, 'twipin/index.html', contexto)
 
+@csrf_exempt
 def api_twips(request):
   if request.method == "GET":
     twips = Twip.objects.all()
@@ -16,4 +18,11 @@ def api_twips(request):
       temp = {"id": t.id, "texto": t.texto, "autor": t.autor.username}
       jt["lista"].append(temp)
 
+    return JsonResponse(jt)
+  elif request.method == "POST":
+    t = request.POST["texto"]
+    usuario = User.objects.get(username='admin')
+    twip = Twip(autor=usuario, texto = t)
+    twip.save()
+    jt = {"mensagem":"ok"}
     return JsonResponse(jt)
